34e6be588760a9d571b8dccd595f2ddff43846b4
denysburimovqte <denysburimovqte@gmail.com>
2025-01-22 07:17:27 +0100

    feat: add image under construction

--- app/apartment/page.tsx
+++ app/apartment/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function EmptyPage() {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gray-300">
-      <h1 className="text-center text-xl text-black">This page is under construct</h1>
+      <Image className="w-full h-full object-cover" src="/images/under-construction.webp" width={800} height={800} alt="page under construction" />
     </div>
   );
 }
